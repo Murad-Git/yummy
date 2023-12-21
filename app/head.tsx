825263907b4bd5ca75456615440b57642d1c976c
Murad-Git <murad.ishmurodov@gmail.com>
2023-12-21 15:07:20 +0100

    changed dependency

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,26 @@ export default function Head() {
     <>
       <title>Yummy Recipes</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='Yummy recipes' />
+      <meta name='description' content='Yummy Recipes' />
       <link rel='icon' href='/logo/restaurant.png' />
+      <meta name='author' content='Murad Kos' key='author' />
+      <meta property='og:title' content='Yummy Recipes' />
+      <meta
+        property='og:description'
+        content='Cook with joy! Easy recipes for everyone, from beginners to pros. Every dish is a masterpiece in the making. Happy cooking!'
+      />
+      <meta
+        property='og:image'
+        content='/images/\background/page-screenshot2.png'
+      />
+      <meta property='og:image:alt' content='Landing page' />
+      <meta
+        property='og:url'
+        content='https://shoppee-e-wnsg.vercel.app/'
+        data-react-helmet='true'
+      />
+      <meta property='og:type' content='website' data-react-helmet='true' />
+      <meta property='fb:app_id' content='857361435508119' />
     </>
   );
 }
